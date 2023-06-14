4fd71bc495a655d8d1b79e7c0f730987262c3eae
Kevin Harp <kharp1984@gmail.com>
2023-06-14 08:55:37 -0400

    users ajax routes done

---
+++ src/ajax-requests/requests.js
@@ -0,0 +1,75 @@
+export const BASE_URL = 'http://fitnesstrac-kr.herokuapp.com/api/';
+
+//* USERS
+
+export const registerUser = async (user) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const registeredUser = async (user) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user,
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const myData = async (token) => {
+    try {
+      const response = await fetch(`${BASE_URL}/users/me`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+      });
+      const result = await response.json();
+      return result
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  export const myRoutineData = async (token) => {
+    try {
+      const response = await fetch(`${BASE_URL}/users/:username/routines`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+      });
+      const result = await response.json();
+      return result
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+//* ACTIVITIES
+
+//* ROUTINES
+
+//* ROUTINE_ACTIVITIES
